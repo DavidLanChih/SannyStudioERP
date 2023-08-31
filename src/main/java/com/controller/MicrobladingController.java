1feb7eed3435e4ffa53c172c9d1cd2bf8d43e45f
DavidLanChih <cljh20220@gmail.com>
2023-09-01 00:12:21 +0800

    解決空白送出故障

--- src/main/java/com/controller/MicrobladingController.java
+++ src/main/java/com/controller/MicrobladingController.java
@@ -65,31 +65,55 @@ public class MicrobladingController {
 		
 		try(Connection con = datasource.getConnection();)
 		{	
-			//取得最新單號
-			int billno=0;
-			String qrySql="select max(s003_Billno) as billno from sal003";
-			Statement qryStmt=con.createStatement();
-			ResultSet qrsRs=qryStmt.executeQuery(qrySql);
-			if(qrsRs.next())
+			if (Data.getName().length()        !=0 &&
+				Data.getSex().length()         !=0 &&
+				Data.getPhone().length()       !=0 &&
+				Data.getServiceItem().length() !=0 &&
+				Data.getSalePrice().length()   !=0 &&
+				Data.getCreateDate().length()  !=0    ) 
 			{
-				billno=qrsRs.getInt("billno");
-				System.out.println(billno);
-				billno++;
+				System.out.println("para ok");
+				//取得最新單號
+				int billno=0;
+				String qrySql="select max(s003_Billno) as billno from sal003";
+				Statement qryStmt=con.createStatement();
+				ResultSet qrsRs=qryStmt.executeQuery(qrySql);
+				if(qrsRs.next())
+				{
+					billno=qrsRs.getInt("billno");
+					System.out.println(billno);
+					billno++;
+				}
+				//匯入資料
+				String insertSql="insert into sal003 ( s003_Billno,"
+													+ "s003_Name,"
+													+ "s003_Sex,"
+													+ "s003_Phone,"
+													+ "s003_ServiceItem,"
+													+ "s003_SalePrice,"
+													+ "s003_CreateDate,"
+													+ "s003_Memo) "
+													+ "values (?,?,?,?,?,?,?,?)";
+				PreparedStatement preStmt=con.prepareStatement(insertSql);
+				preStmt.setInt(1,billno);
+				preStmt.setString(2,Data.getName());
+				preStmt.setString(3,Data.getSex());
+				preStmt.setString(4,Data.getPhone());
+				preStmt.setString(5,Data.getServiceItem());
+				int iPrice=Integer.parseInt(Data.getSalePrice());
+				preStmt.setInt(6,iPrice);
+				preStmt.setString(7,Data.getCreateDate());
+				preStmt.setString(8,Data.getMemo());
+				preStmt.executeUpdate();
+				mod.addAttribute("result", "資料送出成功!");
+				
 			}
-			//匯入資料
-			String insertSql="insert to sal003 ( s003_Billno,"+
-												"s003_Name,"+
-												"s003_Sex,"+
-												"s003_Phone,"+
-												"s003_ServiceItem,"+
-												"s003_SalePrice,"+
-												"s003_CreateDate,"+
-												"s003_Memo) value(????????)";
 			
 		}
 		catch(SQLException e)
 		{
 			System.out.println(e);
+			mod.addAttribute("result", "資料送出失敗，請聯絡負責資訊公司!");
 		}
 		System.out.println("Name: " + Data.getName() +
 				" Sex: " + Data.getSex() +
@@ -98,16 +122,7 @@ public class MicrobladingController {
 				" SaleP: " + Data.getSalePrice() +
 				" Creat: " + Data.getCreateDate() +
 				" Memo: " + Data.getMemo());
-		if (	Data.getName().length()        !=0 &&
-				Data.getSex().length()         !=0 &&
-				Data.getPhone().length()       !=0 &&
-				Data.getServiceItem().length() !=0 &&
-				Data.getSalePrice().length()   !=0 &&
-				Data.getCreateDate().length()  !=0    ) 
-		{
-			System.out.println("ok");
-			
-		}
+		
 		return "orderForm";
 	}
 
