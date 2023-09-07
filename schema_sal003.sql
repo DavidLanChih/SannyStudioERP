289cf253a5ceb93c9ffa9cd91aad623d9b113cda
DavidLanChih <cljh20220@gmail.com>
2023-09-07 11:34:14 +0800

    schema

---
+++ schema_sal003.sql
@@ -0,0 +1,11 @@
+use sannystudio;
+create table sal003 (
+	s003_Billno int primary key not null,
+    s003_Name char(20) not null,
+	s003_Sex char(10) not null,
+	s003_Phone char(10) not null,
+	s003_ServiceItem char(4) not null,
+	s003_SalePrice int not null,
+	s003_CreateDate datetime not null,
+	s003_Memo varchar(200) 
+);
